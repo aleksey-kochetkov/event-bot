c81baa912541085acc075bf989c63420067a7296
akochetkov <akochetkov@ceki.ru>
2023-09-01 10:04:14 +0300

    40108 Добавить в чат-бот ограничения по функциональным возможностям

---
+++ src/main/java/ru/ceki/fgiski2/eventbot/EventBot.java
@@ -0,0 +1,26 @@
+package ru.ceki.fgiski2.eventbot;
+
+import org.springframework.beans.factory.annotation.Value;
+
+@Service
+public class EventBot extends TelegramLongPollingBot {
+    @Value("${event-bot.bot-username}")
+    private String botUsername;
+    @Value("${event-bot.bot-token}")
+    private String botToken;
+    private Queue<QueueElement> consumer;
+
+    @Override
+    public String getBotUsername() {
+        return this.botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return this.botToken;
+    }
+    
+    @Override
+    public void onUpdateReceived(Update update) {
+    }
+}
